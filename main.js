6fb37a85b7d6380ec8986433344fe76df004a268
Sergey <sv.peganov@gmail.com>
2018-12-11 04:40:23 +0300

    Start
    Start

---
+++ main.js
@@ -0,0 +1,14 @@
+$(window).load(function(){
+
+    var winHeight = $(document).height();
+    var step = 4;
+    var timeToScroll = winHeight/step;
+
+    $('.scrolltop').on('click', function(){
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, timeToScroll);
+    });
+
+});
